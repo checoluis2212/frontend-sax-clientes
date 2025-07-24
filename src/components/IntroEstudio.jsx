f8f1b73dd268ec140a3abb12497fefb6cd65e0c6
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-24 08:27:47 -0600

    Update IntroEstudio.jsx

--- src/components/IntroEstudio.jsx
+++ src/components/IntroEstudio.jsx
@@ -4,24 +4,25 @@ import React from 'react';
 export default function IntroEstudio({ onStart }) {
   return (
     <div className="container py-5">
-      <div className="row flex-column-reverse flex-md-row">
-        {/* IZQUIERDA */}
+      <div className="row">
+        {/* IZQUIERDA: título + botón DESKTOP */}
         <div className="col-md-6 mb-4">
           <h1 className="display-4 fw-bold">
-            Solicita un estudio<br/>socioeconómico al instante
+            Solicita un estudio<br />socioeconómico al instante
           </h1>
           <p className="mt-4">
             Estándar $500 MXN | Urgente $800 MXN
           </p>
+          {/* Botón solo en pantallas md+ */}
           <button
-            className="btn btn-primary btn-lg mt-3"
+            className="btn btn-primary btn-lg mt-3 d-none d-md-inline-block"
             onClick={onStart}
           >
             Empezar ahora
           </button>
         </div>
 
-        {/* DERECHA */}
+        {/* DERECHA: pasos */}
         <div className="col-md-6">
           <ul className="list-group">
             <li className="list-group-item mb-3">
@@ -37,6 +38,16 @@ export default function IntroEstudio({ onStart }) {
               <small>Elige Estándar (500 MXN) o Urgente (800 MXN) y finaliza.</small>
             </li>
           </ul>
+
+          {/* Botón solo en xs–sm (mobile) */}
+          <div className="mt-4 d-block d-md-none">
+            <button
+              className="btn btn-primary btn-lg w-100"
+              onClick={onStart}
+            >
+              Empezar ahora
+            </button>
+          </div>
         </div>
       </div>
 
@@ -44,7 +55,7 @@ export default function IntroEstudio({ onStart }) {
       <div className="row justify-content-center mt-5">
         <div className="col text-center">
           <p>
-            ¿Necesitas varios estudios?&nbsp;
+            ¿Necesitas varios estudios?{' '}
             <a
               href="https://wa.me/525512345678?text=Cotización%20volumen"
               target="_blank"
