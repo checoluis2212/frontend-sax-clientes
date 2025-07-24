66740436c3532faba9d40cac66b238adb73aff98
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-24 08:20:55 -0600

    Update IntroEstudio.jsx

--- src/components/IntroEstudio.jsx
+++ src/components/IntroEstudio.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function IntroEstudio({ onStart }) {
   return (
     <div className="container py-5">
-      <div className="row">
+      <div className="row flex-column-reverse flex-md-row">
         {/* IZQUIERDA */}
         <div className="col-md-6 mb-4">
           <h1 className="display-4 fw-bold">
