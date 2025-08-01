f71e710796c6380631d6679af38fb8ed8adde5c7
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-01 09:28:14 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+// src/main.jsx
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './contexts/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
-)
+);
