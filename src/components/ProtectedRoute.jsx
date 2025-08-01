e268254f55bf421b9d0817d3c7b560283173a319
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-01 08:59:06 -0600

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+// src/components/ProtectedRoute.jsx
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth }  from '../contexts/AuthContext';
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  if (loading) return <div>Cargando sesión…</div>;
+  return user ? children : <Navigate to="/login" replace />;
+}
