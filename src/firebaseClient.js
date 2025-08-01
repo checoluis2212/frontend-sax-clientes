a75a35201acf5fdabf67bd47f015828d3fefc2f6
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-01 09:59:10 -0600

    Create firebaseClient.js

---
+++ src/firebaseClient.js
@@ -0,0 +1,15 @@
+// src/firebaseClient.js
+import { initializeApp } from 'firebase/app';
+import { getAuth }       from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey:            'TU_API_KEY',
+  authDomain:        'TU_PROYECTO.firebaseapp.com',
+  projectId:         'TU_PROYECTO',
+  storageBucket:     'TU_PROYECTO.appspot.com',
+  messagingSenderId: 'TU_MENSAJESENDERID',
+  appId:             'TU_APP_ID',
+};
+
+const app  = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
