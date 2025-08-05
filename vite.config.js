c6a7fcc44de82c525d521781d7c6ff28b71ed731
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-05 17:25:53 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🔹 asegura rutas relativas correctas en producción
+  base: './', // 🔹 asegura rutas relativas correctas
 })
